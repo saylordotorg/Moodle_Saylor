81956bbcaea29f5bd549ed35c20b1d8b5a569985
Jenkins <build@saylor.org>
2017-10-25 10:38:41 -0400

    Build #33 - Automated Commit Message

---
+++ local/sayonara/index.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Sayonara
+ *
+ * This fork of Goodbye is designed to work with Moodle 3.2+ and the Boost theme.
+ * The option to delete will be in the user's profile.
+ *
+ * @package    local
+ * @subpackage sayonara
+ * @copyright  2017 Saylor Academy
+ * @author     John Azinheira
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * Goodbye
+ *
+ * This module has been created to provide users the option to delete their account
+ *
+ * @package    local
+ * @subpackage goodbye, delete your moodle account
+ * @copyright  2013 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+require_once($CFG->dirroot . '/local/sayonara/check_account_form.php');
+
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url('/local/sayonara/index.php');
+$PAGE->set_title(format_string(get_string('deleteaccount', 'local_sayonara')));
+$PAGE->set_heading(format_string(get_string('userpass', 'local_sayonara')));
+$enabled = get_config('local_sayonara', 'enabled');
+
+require_login();
+
+if ($enabled && !is_siteadmin() && !isguestuser()) {
+    $checkaccount = new check_account_form();
+
+    global $USER;
+
+    $error = '';
+
+    if ($local_user = $checkaccount->get_data()) {
+        if ($local_user->username != '' && $local_user->password != '') {
+            if ($user = $DB->get_record('user', array('username'=>$local_user->username))) {
+                // User Exists, Check pass.
+                if ($user = authenticate_user_login($local_user->username, $local_user->password) ) {
+                    complete_user_login($user);
+                    if ($user->id == $USER->id && ($USER->auth == 'email' || $USER->auth == 'manual')) {
+                        delete_user($user);
+
+                        $authsequence = get_enabled_auth_plugins(); // auths, in sequence
+                        foreach($authsequence as $authname) {
+                            $authplugin = get_auth_plugin($authname);
+                            $authplugin->logoutpage_hook();
+                        }
+
+                        require_logout();
+                        echo $OUTPUT->header(get_string('deleteaccount', 'local_sayonara'));
+                        echo $OUTPUT->notification(get_string('useraccountdeleted', 'local_sayonara'), 'notifysuccess');
+                        echo $OUTPUT->footer();
+                        exit;
+                    } else {
+                        $error = get_string('noself', 'local_sayonara');
+                    }
+                } else {
+                    $error = get_string('loginerror', 'local_sayonara');
+                }
+            } else {
+                $error = get_string('loginerror', 'local_sayonara');
+            }
+        }
+    }
+
+    echo $OUTPUT->header(get_string('deleteaccount', 'local_sayonara'));
+    $checkaccount->display();
+    echo $error;
+} else {
+    echo $OUTPUT->header(get_string('disabled', 'local_sayonara'));
+}
+
+echo $OUTPUT->footer();
+
+
+
---
+++ local/sayonara/lib.php
@@ -0,0 +1,91 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Sayonara
+ *
+ * This fork of Goodbye is designed to work with Moodle 3.2+ and the Boost theme.
+ * The option to delete will be in the user's profile.
+ *
+ * @package    local
+ * @subpackage sayonara
+ * @copyright  2017 Saylor Academy
+ * @author     John Azinheira
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * Goodbye
+ *
+ * This module has been created to provide users the option to delete their account
+ *
+ * @package    local
+ * @subpackage goodbye, delete your moodle account
+ * @copyright  2013 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+function local_sayonara_extend_navigation(global_navigation $navigation) {
+    global $USER;
+
+    if (!isloggedin() || isguestuser() && !is_siteadmin()) {
+        return '';
+    }
+    $enabled = get_config('local_sayonara', 'enabled');
+
+    if ($enabled && ($USER->auth == 'email' || $USER->auth == 'manual')) {
+
+        $container2 = $navigation->get('myprofile');
+        if (!empty($container2)) {
+            $container2->add(get_string('manageaccount', 'local_sayonara'), new moodle_url('/local/sayonara/index.php'));
+        }
+    }
+}
+
+/**
+ * Add nodes to myprofile page.
+ *
+ * @param \core_user\output\myprofile\tree $tree Tree object
+ * @param stdClass $user user object
+ * @param bool $iscurrentuser
+ * @param stdClass $course Course object
+ *
+ * @return bool
+ */
+function local_sayonara_myprofile_navigation(core_user\output\myprofile\tree $tree, $user, $iscurrentuser, $course) {
+    global $PAGE, $USER;
+
+    $enabled = get_config('local_sayonara', 'enabled');
+    $systemcontext = context_system::instance();
+    $title = get_string('manageaccount', 'local_sayonara');
+    $url = new moodle_url('/local/sayonara/index.php');
+
+    // Should be the same capability checks as editing your own profile in myprofile lib.
+    if ((isloggedin() && !isguestuser($user) && !is_mnet_remote_user($user)) && $iscurrentuser && has_capability('moodle/user:editownprofile', $systemcontext)) {
+        // Only show deletion option if the plugin is enabled and user auth is email or manual.
+        if ($enabled && ($USER->auth == 'email' || $USER->auth == 'manual')) {
+
+            $node = new core_user\output\myprofile\node('miscellaneous', 'sayonara', $title, null,
+                $url);
+            $tree->add_node($node);
+
+            return true;
+        }
+    }
+
+    return false;
+}
