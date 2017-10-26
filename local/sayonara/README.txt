a1e9221230d3ca31fa874e2ff03c80fec5a096f0
Jenkins <build@saylor.org>
2017-10-26 14:52:20 -0400

    Build #122 - Automated Commit Message

---
+++ local/sayonara/README.txt
@@ -0,0 +1,34 @@
+Goodbye Moodle
+
+Package name: local_goodbye
+Copyright: 2013 Bas Brands, www.basbrands.nl
+Author: Bas Brands
+License: http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+
+ABOUT
+
+This plugin allows users to delete their account,
+Currently this only works for the standard email based self registration authentication methods
+
+INSTALLATION
+
+Install this plugin in your /local folder
+Then login as an admin and go to
+Site Administration -> Notifications
+
+This should trigger the installation process
+
+
+CONFIGURATION
+
+You can
+
+Then go to:
+Site Administration -> Plugins -> Local Plugins -> Local Goodby
+
+Enable the plugin
+
+TODO
+
+Add option to delete accounts for other authentication methods
---
+++ local/sayonara/version.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Sayonara
+ *
+ * This fork of Goodbye is designed to work with Moodle 3.2+ and the Boost theme.
+ * The option to delete will be in the user's profile.
+ *
+ * @package    local
+ * @subpackage sayonara
+ * @copyright  2017 Saylor Academy
+ * @author     John Azinheira
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * Goodbye
+ *
+ * This module has been created to provide users the option to delete their account
+ *
+ * @package    local
+ * @subpackage goodbye, delete your moodle account
+ * @copyright  2013 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$plugin->version  = 2017102600;
+$plugin->requires = 2016120500;
+$plugin->release = '1.1.0';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->component = 'local_sayonara';
