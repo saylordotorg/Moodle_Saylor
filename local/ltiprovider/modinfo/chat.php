d89446bc3448a8d3051ebb738277fdd43e08a7f2
Jenkins <build@saylor.org>
2018-08-22 23:30:13 -0400

    Build #145 - Automated Commit Message

--- local/ltiprovider/modinfo/chat.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$extramodinfo = array(
-    "chattime" => time());
-
--- theme/saylor/version.php
+++ theme/saylor/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017060600;
+$plugin->version   = 2017060900;
 $plugin->requires  = 2014110400;
 $plugin->component = 'theme_saylor';
